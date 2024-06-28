44d73f1cbbe7dc4e27ae4bb9f1b6619d78f1a399
root <root@alpine2.my.domain>
2024-06-29 08:48:49 +0900

    bt_cron

---
+++ bt_connect.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/expect -f
+
+set prompt "#"
+set address [lindex $argv 0]
+
+spawn bluetoothctl
+expect -re $prompt
+send "scan on\r"
+send_user "\nSleeping\r"
+sleep 10
+send_user "\nDone sleeping\r"
+send "trust $address\r"
+sleep 1
+send "pair $address\r"
+sleep 1
+send "connect $address\r"
+sleep 1
+send_user "\nShould be paired now.\r"
+send "quit\r"
+expect eof
