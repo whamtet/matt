50e372cdd6e2a86b7bc54c4f7d6deb951099cc74
root <root@alpine.my.domain>
2024-07-10 09:15:54 +0900

    tty7 hack

--- rdp-server.sh
+++ rdp-server.sh
@@ -1,4 +1,6 @@
 apk add xrdp xorgxrdp
 cp $(dirname "$0")/xorg.conf /etc/X11/xrdp
-rc-service xrdp start
-rc-service xrdp-sesman start
+service xrdp start
+service xrdp-sesman start
+# hack to get us an empty tty7
+service lightdm start
