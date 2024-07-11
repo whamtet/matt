baa96ae2275c2499f9566bd3c036668e0860db82
root <root@alpine.my.domain>
2024-07-11 19:52:27 +0900

    setxkbmap

--- rdp-server.sh
+++ rdp-server.sh
@@ -1,6 +1,6 @@
 export BROWSER=chromium
 setup-desktop xfce
-apk add xrdp xorgxrdp
+apk add xrdp xorgxrdp setxkbmap
 cp $(dirname "$0")/xorg.conf /etc/X11/xrdp
 service xrdp start
 service xrdp-sesman start
