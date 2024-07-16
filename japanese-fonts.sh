e60b6e88d6585fef1bb00f9ebcabd6f722147536
Matthew Molloy <whamtet@gmail.com>
2024-07-17 08:55:11 +0900

    Japanese fonts

---
+++ japanese-fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+apk add font-terminus font-inconsolata font-dejavu font-noto font-noto-cjk font-awesome font-noto-extra
+apk add font-noto font-noto-thai font-ipa font-sony-misc font-jis-misc
+
