e07f87378ceaf9984195b16c89e2e94916ebffa2
root <root@alpine.my.domain>
2024-07-23 16:45:43 +0900

    bt_install.sh better

--- keyboard/bt_install.sh
+++ keyboard/bt_install.sh
@@ -1,4 +1,7 @@
 apk add bluez expect
 modprobe btusb
 cp $(dirname $0)/input.conf /etc/bluetooth
-service bluetooth start
+cp $(dirname $0)/keyboard /etc/init.d
+rc-update add bluetooth
+rc-update add keyboard
+lbu include /etc/init.d/keyboard
