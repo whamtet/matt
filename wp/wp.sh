6670a0c2b5bdc08247de4b6276f1258fdac55b19
root <root@localhost.my.domain>
2025-05-23 03:48:12 +0900

    wp

---
+++ wp/wp.sh
@@ -0,0 +1,24 @@
+apk add lighttpd composer fcgi php83-cgi
+cp $(dirname $0)/lighttpd.conf /etc/lighttpd
+apk add wget mysql mysql-client php83-mysqli
+service lighttpd start
+
+mkdir -p /usr/share/webapps/
+
+cd /usr/share/webapps/
+wget https://wordpress.org/latest.tar.gz
+
+tar -xzvf latest.tar.gz
+rm latest.tar.gz
+chown -R lighttpd /usr/share/webapps/
+
+ln -s /usr/share/webapps/wordpress/ /var/www/localhost/htdocs/wordpress
+
+/usr/bin/mysql_install_db --user=mysql
+/etc/init.d/mariadb setup
+rc-service mariadb start && rc-update add mariadb default
+/usr/bin/mysqladmin -u root password 'password'
+
+mysql -u root -ppassword < $(dirname $0)/wp.sql
+
+echo '<?php phpinfo();' > /var/www/localhost/htdocs/index.php
---
+++ wp/wp.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress password';
+FLUSH PRIVILEGES;
+EXIT
