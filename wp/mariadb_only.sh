570c174aa451923b93f000d887de118563a489a1
root <root@localhost.my.domain>
2025-07-25 19:10:55 +0900

    mariadb_only

---
+++ wp/mariadb_only.sh
@@ -0,0 +1,12 @@
+apk add mariadb mariadb-client
+# mariadb-install-db --user=mysql
+/etc/init.d/mariadb setup
+
+# vi /etc/my.cnf.d/mariadb-server.cnf 
+# change skip-networking, bind-address
+
+# service mariadb start
+
+# mariadb
+# GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%' IDENTIFIED BY 'password';
+# FLUSH PRIVILEGES;
